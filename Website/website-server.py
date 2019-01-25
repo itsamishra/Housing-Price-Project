3f66419f74a0ded4a952a88a5e33ac6c3ff37cb4
Ash <ash@gscloudsolutions.com>
2019-01-24 17:52:34 -0800

    Started working on website-server.py, created index.html

--- Website/website-server.py
+++ Website/website-server.py
@@ -1,3 +1,23 @@
-from flask import Flask
+from flask import Flask, send_from_directory
+import requests
 
 app = Flask(__name__)
+
+
+# Homepage
+@app.route('/')
+def homepage():
+    return send_from_directory('static', 'index.html')
+    # return "Home Page"
+
+
+# Test GET request
+@app.route('/prediction')
+def get_prediction():
+    predicted_usd_price = requests.get('http://127.0.0.1:5000/api/prediction?square_footage=9000&num_bed=4&num_bath=4').json()["predicted_usd_price"]
+
+    return str(predicted_usd_price)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
